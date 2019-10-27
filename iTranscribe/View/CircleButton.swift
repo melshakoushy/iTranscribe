c7bbe1e7887a542742ce4440662a0b25f1022b49
Mahmoud Elshakoushy <mahmoudelshakoushy@192.168.30.130>
2019-10-27 11:47:31 -0700

    first commit

---
+++ iTranscribe/View/CircleButton.swift
@@ -0,0 +1,21 @@
+//
+//  CircleButton.swift
+//  iTranscribe
+//
+//  Created by Mahmoud Elshakoushy on 10/27/19.
+//  Copyright © 2019 Mahmoud Elshakoushy. All rights reserved.
+//
+
+import UIKit
+
+class CircleButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
