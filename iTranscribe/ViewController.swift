2824e006f7211cc2cd0f8a0e0c83da063fd399b9
Mahmoud Elshakoushy <mahmoudelshakoushy@mahmouds-mac.local>
2019-10-27 01:21:07 -0700

    Initial Commit

---
+++ iTranscribe/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  iTranscribe
+//
+//  Created by Mahmoud Elshakoushy on 10/27/19.
+//  Copyright © 2019 Mahmoud Elshakoushy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
