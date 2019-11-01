4584aab9eec821e80510940b1ab43dac7409473a
Mahmoud Elshakoushy <mahmoudelshakoushy@Mahmouds-Mac.local>
2019-11-01 14:10:37 -0700

    last

--- iTranscribe/Controller/MainVC.swift
+++ iTranscribe/Controller/MainVC.swift
@@ -7,14 +7,60 @@
 //
 
 import UIKit
+import Speech
+import AVFoundation
 
-class MainVC: UIViewController {
+class MainVC: UIViewController, AVAudioPlayerDelegate {
 
+    // Outlets
+    @IBOutlet weak var activityInducator: UIActivityIndicatorView!
+    @IBOutlet weak var transTxt: UITextView!
+    
+    //Variables
+    var audioPlayer: AVAudioPlayer!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        activityInducator.isHidden = true
+    }
+    
+    func audioPlayerDidFinishPlaying(_ player: AVAudioPlayer, successfully flag: Bool) {
+        player.stop()
+        activityInducator.stopAnimating()
+        activityInducator.isHidden = true
+    }
+    
+    func requestSpeechAuth() {
+        SFSpeechRecognizer.requestAuthorization { ( authStatus) in
+            if authStatus == SFSpeechRecognizerAuthorizationStatus.authorized {
+                if let path = Bundle.main.url(forResource: "Test1", withExtension: "m4a") {
+                    do {
+                        let sound = try AVAudioPlayer(contentsOf: path)
+                        self.audioPlayer = sound
+                        self.audioPlayer.delegate = self
+                        sound.play()
+                    } catch {
+                        print("Error!")
+                    }
+                    let recognizer = SFSpeechRecognizer()
+                    let request = SFSpeechURLRecognitionRequest(url: path)
+                    recognizer?.recognitionTask(with: request) { (result, error) in
+                        if let error = error {
+                            print("There was an error: \(error)")
+                        } else {
+                           self.transTxt.text = result?.bestTranscription.formattedString
+                        }
+                    }
+                }
+            }
+        }
     }
 
-
+    @IBAction func transcribeBtnPressed(_ sender: Any) {
+        activityInducator.isHidden = false
+        activityInducator.startAnimating()
+        requestSpeechAuth()
+    }
+    
 }
 
